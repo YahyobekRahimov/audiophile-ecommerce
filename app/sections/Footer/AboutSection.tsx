50c35a6d67da51118d7fade375db47b1708b1f2c
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-03-01 09:58:35 +0500

    footer completed

--- app/sections/Footer/AboutSection.tsx
+++ app/sections/Footer/AboutSection.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function AboutSection() {
    return (
       <section>
-         <Container className="py-[7.5rem]">
+         <Container className="py-[7.5rem] lg:flex lg:flex-row-reverse gap-[7.8125rem] ">
             <Image
                src="/assets/shared/desktop/image-best-gear.jpg"
                alt="best gear"
@@ -26,21 +26,23 @@ export default function AboutSection() {
                height={1300}
                className="rounded-lg md:hidden"
             />
-            <h2 className="text-3xl uppercase tracking-[1px] font-bold mt-10 mb-8 text-center">
-               Bringing you the{" "}
-               <span className="text-custom-brown">best</span> audio
-               gear
-            </h2>
-            <p className="text-[0.9375rem] text-custom-black text-opacity-50 leading-[1.66em] text-center">
-               Located at the heart of New York City, Audiophile is
-               the premier store for high end headphones, earphones,
-               speakers, and audio accessories. We have a large
-               showroom and luxury demonstration rooms available for
-               you to browse and experience a wide range of our
-               products. Stop by our store to meet some of the
-               fantastic people who make Audiophile the best place to
-               buy your portable audio equipment.
-            </p>
+            <div>
+               <h2 className="text-3xl uppercase tracking-[1px] font-bold mt-10 mb-8 text-center lg:w-[60%] lg:text-start">
+                  Bringing you the{" "}
+                  <span className="text-custom-brown">best</span>{" "}
+                  audio gear
+               </h2>
+               <p className="text-[0.9375rem] text-custom-black text-opacity-50 leading-[1.66em] text-center lg:text-start">
+                  Located at the heart of New York City, Audiophile is
+                  the premier store for high end headphones,
+                  earphones, speakers, and audio accessories. We have
+                  a large showroom and luxury demonstration rooms
+                  available for you to browse and experience a wide
+                  range of our products. Stop by our store to meet
+                  some of the fantastic people who make Audiophile the
+                  best place to buy your portable audio equipment.
+               </p>
+            </div>
          </Container>
       </section>
    );
--- app/sections/Footer/NavSection.tsx
+++ app/sections/Footer/NavSection.tsx
@@ -1,14 +1,18 @@
 import Container from "@/app/components/Container";
 import Logo from "@/app/components/IconComponents/Logo";
 import Link from "next/link";
+import SocialLinks from "./SocialLinks";
 
 export default function NavSection() {
    return (
-      <section className="bg-custom-black-800">
-         <Container>
-            <div>
+      <section className="bg-custom-black-800 relative">
+         <Container className="relative">
+            <div className="w-[101px] h-1 bg-custom-brown absolute top-0 left-[50%] translate-x-[-50%] md:left-10 md:translate-x-0 xl:left-40"></div>
+         </Container>
+         <Container className="pt-[3rem] pb-10">
+            <div className="flex items-center flex-col gap-[3rem] md:items-start xl:flex-row xl:justify-between">
                <Logo />
-               <ul className="uppercase">
+               <ul className="uppercase text-center flex flex-col gap-4 md:flex-row md:gap-[2rem]">
                   <li>
                      <Link
                         className="text-white tracking-[2px] font-bold text-[0.8125rem] hover:text-custom-brown duration-200"
@@ -42,6 +46,23 @@ export default function NavSection() {
                      </Link>
                   </li>
                </ul>
+            </div>
+            <div className="text-white text-opacity-50 flex flex-col items-center gap-[3rem] text-center mt-[3rem] md:text-start md:flex-row md:flex-wrap md:justify-between">
+               <div className="text-[0.9375rem] leading-[1.66em] w-full xl:flex xl:items-end xl:justify-between">
+                  <p className="xl:w-[50%]">
+                     Audiophile is an all in one stop to fulfill your
+                     audio needs. We're a small team of music lovers
+                     and sound specialists who are devoted to helping
+                     you get the most out of personal audio. Come and
+                     visit our demo facility - we’re open 7 days a
+                     week.
+                  </p>
+                  <SocialLinks className="hidden xl:flex xl:flex-row" />
+               </div>
+               <p className="font-bold">
+                  Copyright 2021. All Rights Reserved
+               </p>
+               <SocialLinks className="xl:hidden" />
             </div>
          </Container>
       </section>
